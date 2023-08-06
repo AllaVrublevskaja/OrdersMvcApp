8145968c16e3d7de63f78a789851f36710156a5b
AllaVrublevskaja <allavrublevskaja@yandex.ru>
2023-08-06 15:34:35 +0300

    initial commit

---
+++ src/test/java/org/top/ordersmvcapp/OrdersMvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.top.ordersmvcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersMvcAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
